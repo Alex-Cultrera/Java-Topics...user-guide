e528d909ba934cd1e6a5c717139a8bc32e7d5373
Alex-Cultrera <alexander.cultrera@gmail.com>
2024-07-23 13:08:05 -0400

    another update to multithreading package

--- src/com/coderscampus/multithreading/MultiThreadingExample1.java
+++ src/com/coderscampus/multithreading/MultiThreadingExample1.java
@@ -20,8 +20,16 @@ public class MultiThreadingExample1 {
 					+ Thread.currentThread().getName());
 		}
 	}
+}
 //	Note that:
 //	1. We used a static class in this example which is not advisable
 //	2. This code executes asynchronously 
-	
-}
+//	3. For most part, the code you write will be single threaded and/or synchronous
+//	4. Computers are naturally multi-threaded because of multiple processes running at once
+//	5. a single thread on it's own is a place where computer can do work
+//	6. a CPU can only do one thing at any given time, but what happens is 
+//		it spreads the load of all the threads (or tasks) across however many processors it has
+//		via context switching which means it rapidly switches back and forth between those threads 
+//		within span of a second
+//	7. you can take advantage of the CPU having multiple processors by writing multi-threaded code
+//	8. in essence, multi-threading is all about efficiently running a task
